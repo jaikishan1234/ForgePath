27ec836a523adba71dc2e3a3b52be863e4b1d07d
Jai <jaikishannayak@gmail.com>
2026-06-13 10:47:17 +0530

    Add routing and context for user authentication
    - Added react-router-dom for routing with a Home page.
    - Introduced AppContext for managing user authentication state.
    - Integrated axios for API calls to fetch user data.
    - Implemented react-hot-toast for notifications.
    - Enhanced styling with Tailwind CSS for a better UI experience.
    - Added CORS support in the server for handling cross-origin requests.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+export const server = "https://localhost:5000";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 connectDB().then(() => console.log("DB connected"));
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
